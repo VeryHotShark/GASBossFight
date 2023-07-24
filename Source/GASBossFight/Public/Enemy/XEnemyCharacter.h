dbeeb0de887f112de446cd5c19b2545496efa874
Nguyen Tu Tony <nguyen.tu.tony2000@gmail.com>
2023-07-24 14:52:23 +0200

    Added Rider Link + Base Class Setup

---
+++ Source/GASBossFight/Public/Enemy/XEnemyCharacter.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/XBaseCharacter.h"
+#include "XEnemyCharacter.generated.h"
+
+UCLASS()
+class GASBOSSFIGHT_API AXEnemyCharacter : public AXBaseCharacter
+{
+	GENERATED_BODY()
+};
---
+++ Source/GASBossFight/Public/Player/XPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/XBaseCharacter.h"
+#include "XPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASBOSSFIGHT_API AXPlayerCharacter : public AXBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
